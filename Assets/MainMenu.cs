fea5e94d6f70d70c2fed2a110ae5e86d7749378a
Nicole Gordon <gordonnicole118@outlook.com>
2022-07-17 18:38:54 +0100

    Menu functionality fully implemented with SFX and buttons working -Level transitions when playing -Quit works when it's exported into an application

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,11 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-        public void PlayGame()
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
-
-
-        }
+    public Transform creditsRef;
+    public Transform mainMenuRef;
+    private void Start()
+    {
+        mainMenuRef = GameObject.Find("MainMenu").transform;
+        creditsRef = GameObject.Find("Credits").transform;
+        creditsRef.gameObject.SetActive(false);
     }
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ShowCredits()
+    {
+        creditsRef.gameObject.SetActive(true);
+        mainMenuRef.gameObject.SetActive(false);
+    }
+
+    public void ShowMainMenu()
+    {
+        creditsRef.gameObject.SetActive(false);
+        mainMenuRef.gameObject.SetActive(true);
+    }
+
+}
